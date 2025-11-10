84ee31e831b22f7284500191a8995483f897e4b3
24f3000687 <24f3000687@ds.study.iitm.ac.in>
2025-11-10 18:55:59 +0000

    Overwrited home page and created home, login, and register pages using Jinja2 Templating and also managed backend routes accordingly

--- routes.py
+++ routes.py
@@ -1,9 +1,27 @@
 from app import app
-from flask import render_template
+from flask import render_template,request,redirect,url_for,flash
+from models import *
 @app.route('/',methods=["GET"])
 def home():
     return render_template('home.html')
 
-@app.route("/login")
+@app.route("/register",methods=["GET","POST"])
+def register():
+    if request.method=="POST":
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        exist_user= User.query.filter_by(name=username).first()
+        if exist_user==username:
+            return redirect(url_for('register'))
+        
+        user = User(username=username, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login',methods=["GET","POST"])
 def login():
-    return render_template('home.html')
+    return render_template("login.html")
