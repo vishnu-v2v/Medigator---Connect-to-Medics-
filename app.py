1cfd98ca92f579abfbbe6bf06b5917a4c9969356
24f3000687 <24f3000687@ds.study.iitm.ac.in>
2026-01-09 22:57:29 +0530

    applied security changes

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask,render_template
-from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask import Flask
+from werkzeug.security import generate_password_hash
+from dotenv import load_dotenv
+import os
+load_dotenv()
 from models import *
 
 
 
 app= Flask(__name__)
-app.config["SECRET_KEY"] = "MyKey"
-app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:///HMSDB.db"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"]= os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
